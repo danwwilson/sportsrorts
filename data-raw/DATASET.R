4e9af453be9289d4794767076d11cb4bb73299bd
Dan Wilson <dan@thedatacollective.com.au>
2020-02-07 09:10:25 +1000

    create `consolidate_rorts` to remove repetition in code for creation of each data set. Add in additional grant types

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -13,47 +13,81 @@ scrape_rorts <- function(year, grant) {
     rvest::html_table()
 }
 
+consolidate_rorts <- function(x) {
+  tmp <- x %>%
+    data.table::rbindlist() %>%
+    unique()
+
+  col_names <- names(tmp)
+
+  setnames(tmp, col_names, tolower(gsub(" ", "_", col_names)))
+  tmp[, funding_amount := as.numeric(gsub("[$,]", "", funding_amount))]
+
+  tmp
+}
+
+# Move It AUS - Community Sport Infrastructure (CSI) ----------------------
 grants_csi <- lapply(2008:2020, scrape_rorts, "CSI") %>%
-  rbindlist() %>%
-  unique()
-
-col_names <- names(grants_csi)
-
-setnames(grants_csi, col_names, tolower(gsub(" ", "_", col_names)))
-grants_csi[, funding_amount := as.numeric(gsub("[$,]", "", funding_amount))]
+  consolidate_rorts()
 
 usethis::use_data(grants_csi)
 
+# Move It AUS - Better Ageing (BA) ----------------------------------------
 grants_ba <- lapply(2008:2020, scrape_rorts, "BA") %>%
-  rbindlist() %>%
-  unique()
-
-col_names <- names(grants_ba)
-
-setnames(grants_ba, col_names, tolower(gsub(" ", "_", col_names)))
-grants_ba[, funding_amount := as.numeric(gsub("[$,]", "", funding_amount))]
+  consolidate_rorts()
 
 usethis::use_data(grants_ba)
 
+# Active After-school Communities program (AASC) --------------------------
 grants_aasc <- lapply(2008:2020, scrape_rorts, "AASC") %>%
-  rbindlist() %>%
-  unique()
-
-col_names <- names(grants_aasc)
-
-setnames(grants_aasc, col_names, tolower(gsub(" ", "_", col_names)))
-grants_aasc[, funding_amount := as.numeric(gsub("[$,]", "", funding_amount))]
+  consolidate_rorts()
 
 usethis::use_data(grants_aasc)
 
+# dAIS Athlete Grant (DAS) ------------------------------------------------
 grants_das <- lapply(2008:2020, scrape_rorts, "DAS") %>%
-  rbindlist() %>%
-  unique()
-
-col_names <- names(grants_das)
-
-setnames(grants_das, col_names, tolower(gsub(" ", "_", col_names)))
-grants_das[, funding_amount := as.numeric(gsub("[$,]", "", funding_amount))]
+  consolidate_rorts()
 
 usethis::use_data(grants_das)
 
+# Elite Indigenous Travel and Accommodation Assistance program (EITAAP) ---
+grants_eitaap <- lapply(2008:2020, scrape_rorts, "EITAAP") %>%
+  consolidate_rorts()
+
+usethis::use_data(grants_eitaap)
+
+# Multicultural Youth Sports Partnership Program (MYSP) -------------------
+grants_mysp <- lapply(2008:2020, scrape_rorts, "MYSP") %>%
+  consolidate_rorts()
+
+usethis::use_data(grants_mysp)
+
+# National Sporting Organisation (NSO) ------------------------------------
+grants_nso <- lapply(2008:2020, scrape_rorts, "NSO") %>%
+  consolidate_rorts()
+
+usethis::use_data(grants_nso)
+
+# Move It AUS - Participation (PAR) ---------------------------------------
+grants_par <- lapply(2008:2020, scrape_rorts, "PAR") %>%
+  consolidate_rorts()
+
+usethis::use_data(grants_par)
+
+# State/Territory Organisations (SPD Other) -------------------------------
+grants_spd <- lapply(2008:2020, scrape_rorts, "SPD") %>%
+  consolidate_rorts()
+
+usethis::use_data(grants_spd)
+
+# Sporting Schools --------------------------------------------------------
+grants_ss <- lapply(2008:2020, scrape_rorts, "Sporting+Schools") %>%
+  consolidate_rorts()
+
+usethis::use_data(grants_ss)
+
+# Women Leaders in Sport (WLIS) -------------------------------------------
+grants_wlis <- lapply(2008:2020, scrape_rorts, "WLIS") %>%
+  consolidate_rorts()
+
+usethis::use_data(grants_wlis)
